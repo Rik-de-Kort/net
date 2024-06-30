9adf992e1af12cc70dc350a4f2ebba4c2a3ed550
Rik <rik.de.kort@hey.com>
2024-06-30 19:03:21 +0200

    working 00_echogit status

--- 00_echo/src/main.zig
+++ 00_echo/src/main.zig
@@ -2,41 +2,53 @@ const std = @import("std");
 const posix = std.posix;
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // Find address information for a local socket, then connect?
     const allocator = std.heap.page_allocator;
 
-    const addressList = try std.net.getAddressList(allocator, "localhost", 3491);
+    const addressList = try std.net.getAddressList(allocator, "0.0.0.0", 3491);
     defer addressList.deinit();
     std.debug.print("Got addressList, {any}!\n", .{addressList.addrs});
 
-    const myAddress = addressList.addrs[1];
+    const myAddress = addressList.addrs[0];
     const mySocket = try posix.socket(myAddress.in.sa.family, posix.SOCK.STREAM, 0);
     defer posix.close(mySocket);
+
+    try posix.setsockopt(mySocket, posix.SOL.SOCKET, posix.SO.REUSEADDR, &std.mem.toBytes(@as(c_int, 1)));
 
     std.debug.print("Got socket, {any}!\n", .{mySocket});
     try posix.bind(mySocket, &myAddress.any, myAddress.getOsSockLen());
     std.debug.print("Bound!\n", .{});
-    try posix.connect(mySocket, &myAddress.any, myAddress.getOsSockLen());
+    // try posix.connect(mySocket, &myAddress.any, myAddress.getOsSockLen());
     std.debug.print("Connected! {any} {any}\n", .{ mySocket, 10 });
-    try posix.listen(mySocket, 0);
+    try posix.listen(mySocket, 5);
     std.debug.print("Listening...\n", .{});
-    var remoteAddress: std.net.Address = undefined;
-    const connectionSocket = try posix.accept(mySocket, &remoteAddress.any, null, 0);
-    std.debug.print("Got request from {any} with socket {any}!\n", .{ remoteAddress, connectionSocket });
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    while (true) {
+        var remoteAddress: std.net.Address = undefined;
+        var addressSize = myAddress.getOsSockLen();
+        const connectionSocket = try posix.accept(mySocket, &remoteAddress.any, &addressSize, 0);
+        defer posix.close(connectionSocket);
+        std.debug.print("Got request from {any} with socket {any}!\n", .{ remoteAddress, connectionSocket });
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+        var data = std.ArrayList(u8).init(allocator);
+        defer data.deinit();
+        var smallBuf: [1024]u8 = undefined;
+        while (true) {
+            const nBytesReceived = try posix.recv(connectionSocket, &smallBuf, 0);
+            if (nBytesReceived <= 0) {
+                break;
+            }
+            std.debug.print("Got {any} bytes: {s}\n", .{ nBytesReceived, smallBuf });
 
-    try bw.flush(); // don't forget to flush!
+            try data.appendSlice(smallBuf[0..nBytesReceived]);
+            if (smallBuf[nBytesReceived - 1] < 0) { // EOF
+                break;
+            }
+        }
+        var bytesSent: usize = 0;
+        while (bytesSent < data.items.len) {
+            bytesSent += try posix.send(connectionSocket, data.items[bytesSent..data.items.len], 0);
+        }
+    }
 }
 
 test "simple test" {
