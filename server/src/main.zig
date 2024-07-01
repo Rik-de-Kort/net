6fc6e25819a2952f656fb5f97c6e7e15e2842fa6
Rik de Kort <rik.de.kort@gmail.com>
2024-07-01 22:37:32 +0200

    more complicated server behaviour

--- server/src/main.zig
+++ server/src/main.zig
@@ -3,13 +3,23 @@ const std = @import("std");
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
     const stream = try std.net.tcpConnectToHost(allocator, "localhost", 3491);
+    defer stream.close();
+
     var rng = std.Random.DefaultPrng.init(42);
     const random = rng.random();
-    const num = std.Random.int(random, u12);
-    std.debug.print("Got {any}\n", .{num});
 
-    const nBytesWritten = try stream.write("Hello, world!");
-    std.debug.print("Wrote {any} bytes\n", .{nBytesWritten});
+    // Reusing this buffer seems to encounter some use-after-free :)
+    var msg = std.ArrayList(u8).init(allocator);
+    var i: usize = 0;
+    while (i < 100) : (i += 1) {
+        const num = std.Random.int(random, u12);
+        try std.json.stringify(.{ .method = "isPrime", .number = num }, .{}, msg.writer());
+        try msg.append(10);
+        std.debug.print("Writing {s}\n", .{msg.items});
+        const nBytesWritten = try stream.write(msg.items);
+        std.debug.print("Wrote {any} bytes\n", .{nBytesWritten});
+        msg.clearAndFree();
+    }
 }
 
 test "simple test" {
