467c7581f8f3c42248a130260d2c6de6e4ddde98
Rik <rik.de.kort@hey.com>
2024-07-03 22:48:32 +0200

    progress!

--- 01_prime/src/main.zig
+++ 01_prime/src/main.zig
@@ -1,7 +1,17 @@
 const std = @import("std");
 const posix = std.posix;
 
-const PrimeInput = struct { method: *const [7]u8 = "isPrime", number: f64 };
+pub fn stringEql(a: []const u8, b: []const u8) bool {
+    if (a.len != b.len) {
+        return false;
+    }
+    for (a, b) |a_elem, b_elem| {
+        if (a_elem != b_elem) return false;
+    }
+    return true;
+}
+
+const PrimeInput = struct { method: *const [7]u8, number: f64 };
 
 fn isPrime(x: f64) bool {
     if (x < 0) {
@@ -33,6 +43,7 @@ fn isPrime(x: f64) bool {
 }
 
 fn handle(allocator: std.mem.Allocator, msg: []u8) !std.ArrayList(u8) {
+    // Todo: hand-roll parser
     const parsed = try std.json.parseFromSlice(
         PrimeInput,
         allocator,
@@ -40,6 +51,11 @@ fn handle(allocator: std.mem.Allocator, msg: []u8) !std.ArrayList(u8) {
         .{},
     );
     defer parsed.deinit();
+
+    std.debug.print("Got method {s}\n", .{parsed.value.method});
+    if (!stringEql(parsed.value.method, "isPrime")) {
+        return error.WrongMethod;
+    }
 
     std.debug.print("Parsed {any}\n", .{parsed.value});
     var result = std.ArrayList(u8).init(allocator);
@@ -50,6 +66,10 @@ fn handle(allocator: std.mem.Allocator, msg: []u8) !std.ArrayList(u8) {
 
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
+
+    const wrongMethod = "{\"method\":\"primeis\",\"number\":5337857}";
+    const result2 = try std.json.parseFromSlice(PrimeInput, allocator, wrongMethod, .{});
+    defer result2.deinit();
 
     const address = try std.net.Address.parseIp4("0.0.0.0", 3491);
     std.debug.print("Address: {any}\n", .{address});
@@ -62,6 +82,10 @@ pub fn main() !void {
         std.debug.print("waiting for connection...\n", .{});
         const connection = try server.accept();
         defer connection.stream.close();
+
+        const timeout: std.posix.timeval = .{ .tv_sec = 10, .tv_usec = 0 };
+        try std.posix.setsockopt(connection.stream.handle, std.posix.SOL.SOCKET, std.posix.SO.RCVTIMEO, std.mem.asBytes(&timeout));
+
         connection_loop: while (true) {
             std.debug.print("stream: {any}\n", .{connection});
 
@@ -73,38 +97,49 @@ pub fn main() !void {
 
             read_and_handle: while (true) {
                 std.debug.print("Waiting to read...\n", .{});
-                const nBytesReceived = try connection.stream.read(&receiveBuf);
+                const nBytesReceived = connection.stream.read(&receiveBuf) catch |err| switch (err) {
+                    error.WouldBlock => break :connection_loop,
+                    else => |e| return e,
+                };
                 if (nBytesReceived <= 0) {
-                    break :read_and_handle;
+                    std.debug.print("No bytes received, cleaning up connection\n", .{});
+                    break :connection_loop;
                 }
                 std.debug.print("Got {any} bytes: {s}\n", .{ nBytesReceived, receiveBuf });
                 std.debug.print("as bytes {any}\n", .{receiveBuf});
 
                 try data.appendSlice(receiveBuf[0..nBytesReceived]);
 
-                var j = i; // Index of \n (end of message)
-                while (j < data.items.len) : (j += 1) {
-                    if (data.items[j] == 10) {
-                        break;
+                while (true) {
+                    var j = i; // Index of \n (end of message)
+                    while (j < data.items.len) : (j += 1) {
+                        if (data.items[j] == 10) {
+                            break;
+                        }
+                    } else { // No \n found, fetch more data
+                        continue :read_and_handle;
                     }
-                } else { // No \n found, fetch more data
-                    continue :read_and_handle;
+
+                    const inputMsg = data.items[i..j];
+                    std.debug.print("Handling input {s}\n", .{inputMsg});
+                    std.debug.print("Remaining in memory {s}\n", .{data.items[j..]});
+                    const result = handle(allocator, inputMsg) catch {
+                        std.debug.print("Malformed input, continuing\n", .{});
+                        break :connection_loop;
+                    };
+                    defer result.deinit();
+
+                    std.debug.print("Sending bytes {s}\n", .{result.items});
+                    std.debug.print("Sending bytes (as bytes) {any}\n", .{result.items});
+                    connection.stream.writeAll(result.items) catch |err| switch (err) {
+                        error.ConnectionResetByPeer => break :connection_loop,
+                        error.BrokenPipe => break :connection_loop,
+                        else => |e| return e,
+                    };
+                    std.debug.print("Bytes sent\n", .{});
+
+                    i = j + 1; // j is index of \n, add one to get start of new message
                 }
-
-                const inputMsg = data.items[i..j];
-                std.debug.print("Handling input {s}\n", .{inputMsg});
-                const result = handle(allocator, inputMsg) catch {
-                    std.debug.print("Malformed input, continuing\n", .{});
-                    break :connection_loop;
-                };
-                defer result.deinit();
-
-                std.debug.print("Sending bytes {s}\n", .{result.items});
-                std.debug.print("Sending bytes (as bytes) {any}\n", .{result.items});
-                try connection.stream.writeAll(result.items);
-                std.debug.print("Bytes sent\n", .{});
-
-                i = j + 1; // j is index of \n, add one to get start of new message
             }
         }
     }
@@ -119,4 +154,22 @@ test "isprime function" {
     try expect(!isPrime(121));
     try expect(isPrime(101));
     try expect(isPrime(2));
+}
+
+test "string equal method" {
+    try expect(stringEql("aaaa", "aaaa"));
+    try expect(!stringEql("primeis", "isPrime"));
+}
+
+test "json strictness" {
+    const brackets = "{\"method\":\"isPrime\",\"number\":[5337857]}";
+    try std.testing.expectError(error.UnexpectedToken, std.json.parseFromSlice(PrimeInput, std.testing.allocator, brackets, .{}));
+    const noMethod = "{\"number\":5337857}";
+    try std.testing.expectError(error.MissingField, std.json.parseFromSlice(PrimeInput, std.testing.allocator, noMethod, .{}));
+    const illegalQuotes = "{\"method\":\"isPrime\",\"number\":\"1389564\"}";
+
+    const result = try std.json.parseFromSlice(PrimeInput, std.testing.allocator, illegalQuotes, .{});
+    defer result.deinit();
+    // const wrongMethod = "{\"method\":\"primeis\",\"number\":5337857}";
+    // try std.testing.expectError(error.WrongMethod, std.json.parseFromSlice(PrimeInput, std.testing.allocator, wrongMethod, .{}));
 }
