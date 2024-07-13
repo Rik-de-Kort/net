99ece3f49fa4e13a0ac19ad489394e2e1731458a
Rik <rik.de.kort@hey.com>
2024-07-13 18:08:24 +0200

    implement batching on 02_means

--- 02_means/src/main.zig
+++ 02_means/src/main.zig
@@ -24,7 +24,7 @@ const Insert = packed struct {
     timestamp: i32,
     price: i32,
 
-    fn from_bytes(bytes: [8]u8) Insert {
+    fn from_bytes(bytes: []u8) Insert {
         //std.debug.print("Parsing query message from {x}\n", .{bytes});
         return Insert{
             .timestamp = read_network_int(bytes[0..4].*),
@@ -36,7 +36,7 @@ const Query = packed struct {
     mintime: i32,
     maxtime: i32,
 
-    fn from_bytes(bytes: [8]u8) Query {
+    fn from_bytes(bytes: []u8) Query {
         return Query{
             .mintime = read_network_int(bytes[0..4].*),
             .maxtime = read_network_int(bytes[4..8].*),
@@ -61,6 +61,9 @@ const Database = struct {
             }
         }
         self.data.appendAssumeCapacity(msg);
+        if (self.data.items.len % 1000 == 0) {
+            std.debug.print("{any} items in database\n", .{self.data.items.len});
+        }
     }
 
     pub fn query(self: Database, msg: Query) !i32 {
@@ -99,7 +102,9 @@ const Connection = struct { connection: std.net.Server.Connection, database: Dat
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
+    const gpa_allocator = gpa.allocator();
+    var arena = std.heap.ArenaAllocator.init(gpa_allocator);
+    const allocator = arena.allocator();
 
     const address = try std.net.Address.parseIp4("0.0.0.0", 3491);
     var server = try address.listen(.{ .reuse_address = true, .reuse_port = true, .force_nonblocking = true });
@@ -129,52 +134,65 @@ pub fn main() !void {
 
         // Handle any open connections
         var connections_to_clean = std.ArrayList(usize).init(allocator);
-        per_connection: for (connections.items, databases.items, 0..) |connection, *database, i| {
-            if (database.data.items.len % 1000 == 0) {
-                std.debug.print("Handling connection {any}. Items in database {any}\n", .{ i, database.data.items.len });
-            }
-            var receive_buf: [9]u8 = [_]u8{0} ** 9;
+        per_connection: for (connections.items, databases.items, 0..) |connection, *database, i_conn| {
+            // if (database.data.items.len % 1000 == 0) {
+            // std.debug.print("Handling connection {any}. Items in database {any}\n", .{ i_conn, database.data.items.len });
+            // }
+            var receive_buf: [90000]u8 = [_]u8{0} ** 90000;
             var bytes_read: usize = 0;
-            while (bytes_read < 9) {
-                const n_bytes = connection.stream.read(receive_buf[bytes_read..]) catch |err| switch (err) {
-                    error.WouldBlock => {
-                        //std.debug.print("would block!\n", .{});
+            while (true) {
+                while (bytes_read < 9) {
+                    const n_bytes = connection.stream.read(receive_buf[bytes_read..]) catch |err| switch (err) {
+                        error.WouldBlock => {
+                            std.debug.print("would block!\n", .{});
+                            continue :per_connection;
+                        },
+                        error.NotOpenForReading => {
+                            std.debug.print("not open for reading!\n", .{});
+                            try connections_to_clean.append(i_conn);
+                            continue :per_connection;
+                        },
+                        else => |e| return e,
+                    };
+                    if (n_bytes <= 0) {
+                        std.debug.print("n_bytes {any}\n", .{n_bytes});
+                        try connections_to_clean.append(i_conn);
                         continue :per_connection;
-                    },
-                    error.NotOpenForReading => {
-                        //std.debug.print("not open for reading!\n", .{});
-                        try connections_to_clean.append(i);
-                        continue :per_connection;
-                    },
-                    else => |e| return e,
-                };
-                if (n_bytes <= 0) {
-                    //std.debug.print("n_bytes {any}\n", .{n_bytes});
-                    try connections_to_clean.append(i);
-                    continue :per_connection;
+                    }
+                    bytes_read += n_bytes;
                 }
-                bytes_read += n_bytes;
-            }
-            //std.debug.print("Got {any} bytes: {x}\n", .{ bytes_read, receive_buf });
+                // std.debug.print("Got {any} bytes: {x}\n", .{ bytes_read, receive_buf });
+                std.debug.print("Got {any} bytes\n", .{bytes_read});
+                var i_msg: usize = 0;
+                while (i_msg + 9 <= bytes_read) : (i_msg += 9) {
+                    const msg = receive_buf[i_msg .. i_msg + 9];
 
-            if (receive_buf[0] == 'I') {
-                database.insert(Insert.from_bytes(receive_buf[1..].*)) catch |err| {
-                    std.debug.print("Error handling database insert {any}\n", .{err});
-                    continue :per_connection;
-                };
-            } else if (receive_buf[0] == 'Q') {
-                const result = database.query(Query.from_bytes(receive_buf[1..].*)) catch |err| {
-                    std.debug.print("Error handling database query {any}\n", .{err});
-                    continue :per_connection;
-                };
-                //std.debug.print("Query result {any}, sending...\n", .{result});
-                connection.stream.writeAll(&write_network_int(result)) catch |err| switch (err) {
-                    error.ConnectionResetByPeer, error.BrokenPipe => {
-                        try connections_to_clean.append(i);
-                        continue :per_connection;
-                    },
-                    else => |e| return e,
-                };
+                    if (msg[0] == 'I') {
+                        database.insert(Insert.from_bytes(msg[1..])) catch |err| {
+                            std.debug.print("Error handling database insert {any}\n", .{err});
+                            continue :per_connection;
+                        };
+                    } else if (msg[0] == 'Q') {
+                        const result = database.query(Query.from_bytes(msg[1..])) catch |err| {
+                            std.debug.print("Error handling database query {any}\n", .{err});
+                            continue :per_connection;
+                        };
+                        //std.debug.print("Query result {any}, sending...\n", .{result});
+                        connection.stream.writeAll(&write_network_int(result)) catch |err| switch (err) {
+                            error.ConnectionResetByPeer, error.BrokenPipe => {
+                                try connections_to_clean.append(i_conn);
+                                continue :per_connection;
+                            },
+                            else => |e| return e,
+                        };
+                    }
+                }
+                // Dump remaining content back into receive buf and continue
+                const bytes_remaining = bytes_read - i_msg;
+                for (0..bytes_remaining) |i_receive| {
+                    receive_buf[i_receive] = receive_buf[i_msg + i_receive];
+                }
+                bytes_read = bytes_remaining;
             }
         }
 
